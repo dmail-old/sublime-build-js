ed0f0e9903e0b01d95473898795c7ce799daa4d9
Damien Maillard <dmaillard06@gmail.com>
2016-01-10 11:55:17 +0100

    test everything

--- index.js
+++ index.js
@@ -3,14 +3,18 @@
 function run(filePath){
 	filePath = filePath.replace(/\\/g, '/');
 
+	require('system-test/bin/run');
+
+	/*
 	if( filePath.indexOf('/test/') > -1 ){
-		console.log('test', filePath);
+		//console.log('test', filePath);
 		require('system-test/bin/run');
 	}
 	else{
-		console.log('platform', filePath);
+		//console.log('platform', filePath);
 		require('system-platform/bin/run');
 	}
+	*/
 }
 
 module.exports = run;
